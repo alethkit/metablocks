d4f5296ad78f4ebe3602beea41c59837b32e4190
Tomas Petricek <tomas@tomasp.net>
2024-07-19 15:09:01 +0200

    Add metablocks notes

---
+++ notes/demo.fsx
@@ -0,0 +1,47 @@
+(*
+The following is pseudo-JSON representation of a part of a "metablocks" logo
+grammar (which is a list of definitions that have names and choice of tokens)
+
+[
+  { "name":"<varname>",
+    "tokens": [
+       { "kind":"primitive", "primitive": "string"  }
+    ] },
+  { "name":"<expr>",
+    "choice": [
+       [ { "kind":"reference", "to":"<varname>" } ],
+       [ { "kind":"primitive", "primitive": "number"  } ],
+       [ { "kind":"reference", "to":"<expr>" },
+         { "kind":"character", "value":"+" },
+         { "kind":"reference", "to":"<expr>" } ]
+    ] }
+]*)
+
+// The following is an F# type definition for the above example (roughly)
+type BlockKindName = string
+type PrimitiveType = String | Number
+type BlockElement = 
+  | Literal of string
+  | Primitive of PrimitiveType
+  | Hole of BlockKindName
+  | ListHole of BlockKindName
+
+type Block = BlockElement list
+type BlockKind = { name:BlockKindName; choice:Block list }
+type BNF = BlockKind list
+
+// Now we can define the grammar of LOGO-like langauge using the above
+let logo : BNF = 
+  [ { name="<varname>"; choice=[ [ Primitive(String) ] ] }
+    { name="<expr>"; choice=[
+        [ Hole("<varname>") ]
+        [ Primitive(Number) ]
+        [ Hole("<expr>"); Literal("+"); Hole("<expr>") ]
+    ]}
+    { name="<program>"; choice=[
+        [ ListHole("<cmd>") ]
+    ] }
+  ]
+
+// The quest is to turn (a more complete) description like the above
+// into a nice block based visual editor...
