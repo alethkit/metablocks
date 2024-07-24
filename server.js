24852815157880bdce7a5bd24f81933a3201a91e
alethkit <git@alethk.it>
2024-07-24 16:49:51 +0200

    reduced load time by serving monaco editor locally

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-const { file } = require('bun');
+const { file } = require("bun");
 
 const server = Bun.serve({
   port: 3000,
   async fetch(req) {
     const url = new URL(req.url);
 
-    if (url.pathname === '/') {
-      return new Response(await file('./src/index.html').text(), {
-        headers: { 'Content-Type': 'text/html' },
+    if (url.pathname === "/") {
+      return new Response(await file("./src/index.html").text(), {
+        headers: { "Content-Type": "text/html" },
       });
     }
 
-    if (url.pathname === '/index.js') {
-      return new Response(await file('./build/index.js').arrayBuffer(), {
-        headers: { 'Content-Type': 'application/javascript' },
+    if (url.pathname === "/index.js") {
+      return new Response(await file("./build/index.js").arrayBuffer(), {
+        headers: { "Content-Type": "application/javascript" },
       });
     }
 
-    return new Response('Not Found', { status: 404 });
+    if (url.pathname === "/index.js.map") {
+      return new Response(await file("./build/index.js.map").arrayBuffer(), {
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    return new Response(Bun.file(new URL(req.url).pathname.slice(1)));
+  },
+  error() {
+    return new Response("Page not found", { status: 404 });
   },
 });
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import * as Blockly from "blockly";
 import * as libraryBlocks from "blockly/blocks";
 import { javascriptGenerator } from "blockly/javascript";
 import * as En from "blockly/msg/en";
-import { createPlayground } from "@blockly/dev-tools";
+import { createPlayground, toolboxCategories } from "@blockly/dev-tools";
+import { category as metaBlocksCategory } from "./blocks";
 
 Blockly.setLocale(En);
 
 // Write main module code here, or as a separate file with a "src" attribute on the module script.
 console.log(Blockly, libraryBlocks, javascriptGenerator, En);
 
-const options = {};
+//toolboxCategories.contents += metaBlocksCategory;
+
+const options = {
+  toolbox: toolboxCategories,
+};
 createPlayground(
   document.getElementById("blocklyDiv"),
   (blocklyDiv, options) => {
